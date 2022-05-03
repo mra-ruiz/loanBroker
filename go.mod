c28c83b13d59577cc40ec17be49b42d027decde1
Maria Camila Ruiz Cardenas <Maria.ruiz.cardenas@ibm.com>
2022-05-03 17:15:06 -0400

    fix: main.go runs with hard-coded order and minimal imports

--- go.mod
+++ go.mod
@@ -3,34 +3,26 @@ module e-commerce-app
 go 1.18
 
 require (
-	github.com/aws/aws-lambda-go v1.31.1
-	github.com/aws/aws-sdk-go v1.44.5
-	github.com/aws/aws-xray-sdk-go v1.7.0
+	github.com/cloudevents/sdk-go v1.2.0
 	github.com/cloudevents/sdk-go/v2 v2.9.0
 	github.com/gofrs/uuid v4.2.0+incompatible
 )
 
 require (
-	github.com/andybalholm/brotli v1.0.4 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/hashicorp/golang-lru v0.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.15.2 // indirect
+	github.com/lightstep/tracecontext.go v0.0.0-20181129014701-1757c391b1ac // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/valyala/bytebufferpool v1.0.0 // indirect
-	github.com/valyala/fasthttp v1.36.0 // indirect
+	go.opencensus.io v0.22.0 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.21.0 // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
 	golang.org/x/sys v0.0.0-20220502124256-b6088ccd6cba // indirect
-	golang.org/x/text v0.3.7 // indirect
-	google.golang.org/genproto v0.0.0-20220502173005-c8bf987b8c21 // indirect
-	google.golang.org/grpc v1.46.0 // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
 )
 
 replace aws-step-functions-long-lived-transactions/models => ../models
--- inventory-release/main.go
+++ inventory-release/main.go
@@ -6,40 +6,43 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
+	"time"
 
 	"e-commerce-app/models" // local
 
-	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-xray-sdk-go/xray"
+	cloudevents "github.com/cloudevents/sdk-go"
 )
 
-var dynamoDB *dynamodb.DynamoDB
+// var dynamoDB *dynamodb.DynamoDB
 
-func init() {
+// Hard coding Order
+var myItem1 = models.Item{ItemID: "itemID456", Qty: 1, Description: "Pencil", UnitPrice: 2.50}
+var myItem2 = models.Item{ItemID: "itemID789", Qty: 1, Description: "Paper", UnitPrice: 4.00}
 
-	// Create DynamoDB client
-	var awscfg = &aws.Config{
-		Region: aws.String(os.Getenv("AWS_REGION")),
-	}
-	var sess = session.Must(session.NewSession(awscfg))
-	dynamoDB = dynamodb.New(sess)
 
-	// AWS X-Ray for AWS SDK trace
-	xray.AWS(dynamoDB.Client)
-
-	log.SetPrefix("TRACE: ")
-	log.SetFlags(log.Ldate | log.Ltime)
-
+var myOrder = models.Order{
+	OrderID: "orderID123456", 
+	OrderDate: time.Date(2022, time.January, 1, 2, 30, 50, 12, time.Now().Location()), 
+	CustomerID: "id001", 
+	OrderStatus: "processing", 
+	Items: []models.Item{ myItem1, myItem2 }, 
+	Payment: models.Payment{
+		MerchantID: "merchantID1234", 
+		PaymentAmount: 6.50, 
+		TransactionID: "transactionID7845764", 
+		TransactionDate: "01-1-2022", 
+		OrderID: "orderID123456", 
+		PaymentType: "creditcard"}, 
+	Inventory: models.Inventory{
+		TransactionID: "transactionID7845764", 
+		TransactionDate: "01-1-2022", 
+		OrderID: "orderID123456", 
+		OrderItems: []string{"Pencil", "Paper"}, 
+		TransactionType: "online"},
 }
 
 func handler(ctx context.Context, ord models.Order) (models.Order, error) {
-
+	fmt.Println()
 	log.Printf("[%s] - processing inventory release", ord.OrderID)
 
 	// Find inventory transaction
@@ -49,10 +52,14 @@ func handler(ctx context.Context, ord models.Order) (models.Order, error) {
 		return models.Order{}, models.NewErrReleaseInventory(err.Error())
 	}
 
+	fmt.Println("\nInventory after getTransaction(): \n", inventory)
+
 	// release the items to the inventory
 	inventory.Release()
 
-	// save the inventory transaction
+	fmt.Println("\nInventory after Release(): \n", inventory)
+
+	// // save the inventory transaction
 	err = saveTransaction(ctx, inventory)
 	if err != nil {
 		log.Printf("[%s] - error! %s", ord.OrderID, err.Error())
@@ -66,6 +73,7 @@ func handler(ctx context.Context, ord models.Order) (models.Order, error) {
 		return ord, models.NewErrReleaseInventory("Unable to release inventory for order " + ord.OrderID)
 	}
 
+	fmt.Println()
 	log.Printf("[%s] - reservation processed", ord.OrderID)
 
 	return ord, nil
@@ -75,52 +83,71 @@ func getTransaction(ctx context.Context, orderID string) (models.Inventory, erro
 
 	inventory := models.Inventory{}
 
-	input := &dynamodb.QueryInput{
-		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
-			":v1": {
-				S: aws.String(orderID),
-			},
-			":v2": {
-				S: aws.String("Reserve"),
-			},
-		},
-		KeyConditionExpression: aws.String("order_id = :v1 AND transaction_type = :v2"),
-		TableName:              aws.String(os.Getenv("TABLE_NAME")),
-		IndexName:              aws.String("orderIDIndex"),
-	}
+	// // defining a query input type
+	// input := &dynamodb.QueryInput{
+	// 	ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+	// 		":v1": {
+	// 			S: aws.String(orderID),
+	// 		},
+	// 		":v2": {
+	// 			S: aws.String("Reserve"),
+	// 		},
+	// 	},
+	// 	KeyConditionExpression: aws.String("order_id = :v1 AND transaction_type = :v2"),
+	// 	TableName:              aws.String(os.Getenv("TABLE_NAME")),
+	// 	IndexName:              aws.String("orderIDIndex"),
+	// }
 
-	// Get payment transaction from database
-	result, err := dynamoDB.QueryWithContext(ctx, input)
-	if err != nil {
-		return inventory, err
-	}
+	// // Get payment transaction from database
+	// result, err := dynamoDB.QueryWithContext(ctx, input)
+	// if err != nil {
+	// 	return inventory, err
+	// }
 
-	err = dynamodbattribute.UnmarshalMap(result.Items[0], &inventory)
-	if err != nil {
-		return inventory, fmt.Errorf("failed to DynamoDB unmarshal Record, %v", err.(awserr.Error))
+	// err = dynamodbattribute.UnmarshalMap(result.Items[0], &inventory)
+	// if err != nil {
+	// 	return inventory, fmt.Errorf("failed to DynamoDB unmarshal Record, %v", err.(awserr.Error))
+	// }
+	
+	// fake query for now :)
+	if myOrder.Inventory.OrderID == orderID {
+		inventory = myOrder.Inventory
 	}
-
+	
 	return inventory, nil
 }
 
 func saveTransaction(ctx context.Context, inventory models.Inventory) error {
 
-	marshalledInventory, err := dynamodbattribute.MarshalMap(inventory)
-	if err != nil {
-		return fmt.Errorf("failed to DynamoDB marshal Inventory, %v", err)
-	}
+	// marshalledInventory, err := dynamodbattribute.MarshalMap(inventory)
+	// if err != nil {
+	// 	return fmt.Errorf("failed to DynamoDB marshal Inventory, %v", err)
+	// }
 
-	_, err = dynamoDB.PutItemWithContext(ctx, &dynamodb.PutItemInput{
-		TableName: aws.String(os.Getenv("TABLE_NAME")),
-		Item:      marshalledInventory,
-	})
+	// _, err = dynamoDB.PutItemWithContext(ctx, &dynamodb.PutItemInput{
+	// 	TableName: aws.String(os.Getenv("TABLE_NAME")),
+	// 	Item:      marshalledInventory,
+	// })
 
-	if err != nil {
-		return fmt.Errorf("failed to put record to DynamoDB, %v", err)
-	}
+	// if err != nil {
+	// 	return fmt.Errorf("failed to put record to DynamoDB, %v", err)
+	// }
 	return nil
 }
 
 func main() {
-	lambda.Start(handler)
+
+	fmt.Println("Order: \n", myOrder)
+
+	// Set a target.
+	ctx := cloudevents.ContextWithTarget(context.Background(), "http://localhost:8080/")
+
+	// will delete this and have it work with knative serving instead ---> lambda.Start(HandleRequest)
+	result, err := handler(ctx, myOrder)
+
+	if err != nil {
+		log.Fatalf("some error, %v", err)
+	}
+
+	fmt.Println("\n", result)
 }
