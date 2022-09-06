5851d8caf266493cee2bed25d8ff7c8df9a4bb1e
Maria Camila Ruiz Cardenas <Maria.ruiz.cardenas@ibm.com>
2022-09-06 15:04:10 -0400

    fix: Updated refund/ function

--- refund/main.go
+++ refund/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -12,143 +13,112 @@ import (
 	"e-commerce-app/utils"
 )
 
+var (
+    db *sql.DB
+)
+ 
 func main() {
-	fmt.Println("Starting credit card refund ...")
-	http.HandleFunc("/", prepareData)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+    connectDb()
+
+    http.HandleFunc("/", handler)
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
 }
 
-func prepareData(w http.ResponseWriter, req *http.Request) {
-	// reading headers
-	for k, v := range req.Header {
-		fmt.Printf("%s=%s\n", k, v[0])
-	}
-
-	body, err := io.ReadAll(req.Body)
-	if err != nil {
-		fmt.Printf("Error with io.ReadAll() in prepareData(): : %v", err)
-		log.Println(err)
-		return
-	}
-	defer req.Body.Close()
-	fmt.Println("#####################################")
-	fmt.Println(string(body))
-
-	db, err := utils.ConnectDatabase()
-	if err != nil {
-		fmt.Printf("In prepareData(): Could not connect to database: %v", err)
-		return
-	}
-
-	allStoredOrders := utils.ImportDbData(db)
-	var returned_stored_order models.StoredOrder
-	var bytesRetSto []byte
-
-	for i := range allStoredOrders {
-		returned_stored_order, err = handler(allStoredOrders[i], db)
-		if err != nil {
-			fmt.Printf("Error with handler() in prepareData(): : %v", err)
-			return
-		}
-		// converting returned stored order into a byte slice
-		bytesRetSto, err = json.Marshal(returned_stored_order)
-		fmt.Printf("returned stored order:\n%v", returned_stored_order)
-		w.Write(bytesRetSto)
-	}
+func connectDb() {
+    var err error
+    db, err = utils.ConnectDatabase()
+    if err != nil {
+        fmt.Printf("Could not connect to database: %v", err)
+        log.Fatal(err)
+    }
 }
 
-func handler(storedOrder models.StoredOrder, db *sql.DB) (models.StoredOrder, error) {
+func handler(w http.ResponseWriter, req *http.Request) {
+    body, err := io.ReadAll(req.Body)
+    if err != nil {
+        msg := fmt.Sprintf("Failed to read the request body: %v", err)
+        w.Write([]byte(msg))
+        w.WriteHeader(500) 
+        return
+    }
+    defer req.Body.Close()
 
-	fmt.Println("#####################################")
+    // Receive order to refund
+    var	order models.StoredOrder
+    err = json.Unmarshal(body, &order)
+    if err != nil {
+        msg := fmt.Sprintf("Failed to unmarshal body: %v", err)
+        log.Println(msg)
+        w.Write([]byte(msg))
+        w.WriteHeader(500) 
+        return
+    }
 
-	log.Printf("[%s] - processing refund", storedOrder.OrderID)
+    log.Printf("[%s] - processing refund", order.OrderID)
 
-	// Find Payment transaction for this order
-	payment, err := getTransaction(storedOrder.OrderID, db)
-	if err != nil {
-		log.Printf("[%s] - error! %s", storedOrder.OrderID, err.Error())
-		return storedOrder, models.NewErrProcessRefund(err.Error())
-	}
+    // Find Payment transaction for this order
+    fetchedPayment, err := getPayment(order.OrderID)
+    if err != nil {
+        msg := fmt.Sprintf("handler(): Could not get payment: %v", err)
+        log.Println(msg)
+        w.Write([]byte(msg))
+        w.WriteHeader(500) 
+        return
+    }
 
-	// Process the refund for the order
-	payment.Refund()
+    // Process the refund for the order
+    fetchedPayment.Refund()
 
-	// Saves refunded transaction to the database
-	err = saveTransaction(payment, db)
-	if err != nil {
-		log.Printf("[%s] - error! %s", storedOrder.OrderID, err.Error())
-		return storedOrder, models.NewErrProcessRefund(err.Error())
-	}
+    // Saves refunded transaction to the database
+    err = savePayment(fetchedPayment, order.OrderID)
+    if err != nil {
+        msg := fmt.Sprintf("handler(): Could not save payment: %v", err)
+        log.Println(msg)
+        w.Write([]byte(msg))
+        w.WriteHeader(500) 
+        return
+    }
 
-	// save state
-	storedOrder.Order.Payment = payment
-
-	fmt.Println()
-	log.Printf("[%s] - refund processed", storedOrder.OrderID)
-	resultStoredOrder := storedOrder
-
-	fmt.Println("\nUpdated stored orders:")
-	err = utils.ViewDatabase(db)
-	if err != nil {
-		fmt.Printf("Error with ViewDatabase() in handler(): %v", err)
-		return models.StoredOrder{}, fmt.Errorf("Error with ViewDatabase() in handler(): %w", err)
-	}
-
-	// Only for restoring database for testing reasons
-	err = utils.ResetOrderPayment(db, storedOrder.OrderID)
-	if err != nil {
-		fmt.Printf("Error with ResetOrderPayment() in handler(): %v", err)
-		return models.StoredOrder{}, fmt.Errorf("Error with ResetOrderPayment() in handler(): %w", err)
-	}
-	fmt.Println("\nStored orders after reset:")
-	err = utils.ViewDatabase(db)
-	if err != nil {
-		fmt.Printf("Error with ViewDatabase() in handler(): %v", err)
-		return models.StoredOrder{}, fmt.Errorf("Error with ViewDatabase() in handler(): %w", err)
-	}
-
-	// close database
-	defer db.Close()
-	return resultStoredOrder, nil
+    log.Printf("[%s] - refund processed", order.OrderID)
 }
 
-func getTransaction(orderID string, db *sql.DB) (models.Payment, error) {
-	// Searching for payment
-	resultingPayment, err := db.Query(`select order_info -> 'payment' from stored_orders where order_id = $1;`, orderID)
-	if err != nil {
-		fmt.Printf("getTransaction(): Could not search for payment %v", err)
-		return models.Payment{}, fmt.Errorf("getTransaction(): Could not search for payment %w", err)
-	}
+func getPayment(orderID string) (models.Payment, error) {
+    // Searching for order
+    resultingOrder, err := db.Query(`select order_info from stored_orders where order_id = $1;`, orderID)
+    if err != nil {
+        msg := fmt.Sprintf("Error with Query() in getPayment(): Could not get order: %v", err)
+        log.Println(msg)
+        return models.Payment{}, errors.New(msg)
+    }
 
-	// Convert payment of type JSONB to type models.Payment
-	var payment models.Payment
-	var result []uint8
+    // Convert order of type JSONB to type models.StoredOrder.Order
+    var order models.Order
+    for resultingOrder.Next() {
+        resultingOrder.Scan(&order)
+    }
 
-	for resultingPayment.Next() {
-		resultingPayment.Scan(&result)
-		json.Unmarshal(result, &payment)		
-	}
-	
-	return payment, nil
+    return order.Payment, nil
 }
 
-func saveTransaction(payment models.Payment, db *sql.DB) error {
-	// converting payment into a byte slice
-	paymentBytes, err := json.Marshal(payment)
-	if err != nil {
-		fmt.Printf("saveTransaction(): Could not marshall payment: %v", err)
-		return fmt.Errorf("saveTransaction(): Could not marshall payment: %w", err)
-	}
+func savePayment(payment models.Payment, orderID string) error {
+    // converting payment into a byte slice
+    paymentBytes, err := json.Marshal(payment)
+    if err != nil {
+        msg := fmt.Sprintf("Error with Marshall() in savePayment(): Could not marshall payment: %v", err)
+        log.Println(msg)
+        return errors.New(msg)
+    }
 
-	// Updating payment of specific order
-	updateString := `UPDATE stored_orders SET order_info = jsonb_set(order_info, '{payment}', to_jsonb($1::JSONB), true) WHERE order_id = $2;`
-	_, err = db.Exec(updateString, paymentBytes, payment.OrderID)
-	if err != nil {
-		fmt.Printf("Error with Exec() in saveTransaction(): Could not update payment: %v", err)
-		return fmt.Errorf("Error with Exec() in saveTransaction(): Could not update payment: %w", err)
-	}
+    // Updating payment of specific order
+    updateString := `UPDATE stored_orders SET order_info = jsonb_set(order_info, '{payment}', to_jsonb($1::JSONB), true) WHERE order_id = $2;`
+    _, err = db.Exec(updateString, paymentBytes, orderID)
+    if err != nil {
+        msg := fmt.Sprintf("Error with Exec() in savePayment(): Could not update payment: %v", err)
+        log.Println(msg)
+        return errors.New(msg)
+    }
 
-	return nil
+    return nil
 }
--- refund/main_test.go
+++ refund/main_test.go
@@ -1,89 +1,153 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"fmt"
-	"os"
+	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"e-commerce-app/models"
 	"e-commerce-app/utils"
 
+	"github.com/lib/pq"
 	"github.com/stretchr/testify/assert"
 )
 
-// Test Orders
-var scenarioErrProcessRefund = "../test/order4.json"
-var scenarioSuccessfulOrder = "../test/order7.json"
+const (
+    testOrder = `
+    {
+        "order_id": "orderID123456", 
+        "order_info": 
+        {
+            "order_date": "2022-01-01T02:30:50Z",
+            "customer_id": "id001",
+            "order_status": "processing",
+            "items": [
+                {
+                "item_id": "itemID456",
+                "qty": 1,
+                "description": "Pencil",
+                "unit_price": 2.5
+                },
+                {
+                "item_id": "itemID789",
+                "qty": 1,
+                "description": "Paper",
+                "unit_price": 4.0
+                }
+            ],
+            "payment": {
+                "merchant_id": "merchantID1234",
+                "payment_amount": 6.5,
+                "transaction_id": "transactionID7845764",
+                "transaction_date": "01-1-2022",
+                "order_id": "orderID123456",
+                "payment_type": "creditcard"
+            },
+            "inventory": {
+                "transaction_id": "transactionID7845764",
+                "transaction_date": "01-1-2022",
+                "order_id": "orderID123456",
+                "items": ["itemID456", "itemID789"],
+                "transaction_type": "online"
+            }
+        }
+    }`
+)
 
 func TestHandler(t *testing.T) {
-	assert := assert.New(t)
+    assert := assert.New(t)
 
-	t.Run("ProcessRefund", func(t *testing.T) {
+    t.Run("ProcessRefund", func(t *testing.T) {
 
-		sto_ord := parseOrder(scenarioSuccessfulOrder)
-		db, err := utils.ConnectDatabase()
-		if err != nil {
-			fmt.Printf("TestHandler(): Error with ConnectDatabase(): %v", err)
-		}
-		prepareTestData(db, sto_ord)
+        prepareDb(t)
+        createTable(t)
+        stoOrd := prepareTestData(t)
 
-		stored_order, err := handler(sto_ord, db)
-		if err != nil {
-			t.Fatal("Error failed to trigger with an invalid request")
-		}
+        req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(testOrder))
+        resp := httptest.NewRecorder()
 
-		assert.NotEmpty(stored_order.Order.Payment.TransactionID, "OrderID must be empty")
-		assert.True(stored_order.Order.Payment.PaymentType == "Credit", "PaymentType must not be 'Credit'")
-	})
+        handler(resp, req)
 
+        // Check database
+        test(t, assert, stoOrd)
+
+        // Clean up
+        cleanUp(t)
+    })
 }
 
-func TestError(t *testing.T) {
-	assert := assert.New(t)
-	t.Run("ErrProcessRefund", func(t *testing.T) {
+func prepareDb(t *testing.T) {
+    utils.CredsLocation = "../test/postgres-creds.json"
+    utils.SSLMode = "disable"
 
-		sto_ord := parseOrder(scenarioErrProcessRefund)
-		db, err := utils.ConnectDatabase()
-		if err != nil {
-			fmt.Printf("TestError(): Error with ConnectDatabase(): %v", err)
-		}
-		prepareTestData(db, sto_ord)
-
-		stored_order, err := handler(sto_ord, db)
-		if err != nil {
-			fmt.Print(err)
-		}
-
-		assert.NotEmpty(stored_order.OrderID)
-	})
+    var err error
+    db, err = utils.ConnectDatabase()
+    if err != nil {
+        t.Fatalf("error connecting to the db: %v", err)
+    }
 }
 
-func parseOrder(filename string) models.StoredOrder {
-	inputFile, err := os.Open(filename)
-	if err != nil {
-		println("parseOrder(): opening input file", err.Error())
-	}
-
-	defer inputFile.Close()
-
-	jsonParser := json.NewDecoder(inputFile)
-
-	stored_order := models.StoredOrder{}
-	if err = jsonParser.Decode(&stored_order); err != nil {
-		println("parseOrder(): parsing input file", err.Error())
-	}
-
-	return stored_order
+func createTable(t *testing.T) {
+    _, err := db.Exec(`CREATE TABLE stored_orders (order_id text, order_info JSONB);`)
+    if err, ok := err.(*pq.Error); ok && err.Code.Name() != "duplicate_table" {
+        t.Fatalf("createTable(): error creating table %v", err)
+    }
 }
 
-func prepareTestData(db *sql.DB, sto_ord models.StoredOrder) {
-	order_id := sto_ord.OrderID
-	order_info := sto_ord.Order
-	command := `UPDATE stored_orders SET order_id = $1, order_info = $2;`
-	_, err := db.Exec(command, order_id, order_info)
-	if err != nil {
-		fmt.Printf("prepareTestData(): Error updating database: %v", err)
-	}
+func prepareTestData(t *testing.T) models.StoredOrder {
+    // Parsing test data prior to calling handler()
+    stoOrd := models.StoredOrder{}
+    err := json.Unmarshal([]byte(testOrder), &stoOrd)
+    if err != nil {
+        t.Fatalf("prepareTestData(): error with json unmarshall: %v", err)
+    }
+
+    insertCommand := `INSERT INTO stored_orders (order_id, order_info) VALUES ($1, $2)`
+    _, err = db.Exec(insertCommand, stoOrd.OrderID, stoOrd.Order)
+    if err != nil {
+        fmt.Printf("prepareTestData(): Could not insert test data into database: %v", err)
+    }
+
+    return stoOrd
+}
+
+func test(t *testing.T, a *assert.Assertions, stoOrd models.StoredOrder) {
+    // Fetching test data from test database after calling handler()
+    var allOrderInfos []models.StoredOrder
+    var storedOrder models.StoredOrder
+    rows, err := db.Query(`SELECT * FROM stored_orders WHERE order_id=$1`, stoOrd.OrderID)
+    if err != nil {
+        t.Fatalf("test(): error with query: %v", err)
+    }
+
+    // Parsing data from database
+    for rows.Next() {
+        if err = rows.Scan(&storedOrder.OrderID, &storedOrder.Order); err != nil {
+            if err != nil {
+                t.Fatalf("test(): error with scan: %v", err)
+            }
+        } 
+        // Scan worked, so run asserts        
+        a.True(storedOrder.Order.Payment.TransactionID != stoOrd.Order.Payment.TransactionID, "TransactionID should be modified")
+        a.True(storedOrder.Order.Payment.TransactionDate != stoOrd.Order.Payment.TransactionDate, "TransactionDate should be modified")
+        a.True(storedOrder.Order.Payment.PaymentAmount == (stoOrd.Order.Payment.PaymentAmount)*-1, "Payment amount is not -originalAmount")
+        a.True(storedOrder.Order.Payment.PaymentType == "Credit", "PaymentType should be equal to 'Credit'")
+        allOrderInfos = append(allOrderInfos, storedOrder)
+    }
+    fmt.Println("Printing all orders:\n", allOrderInfos)
+}
+
+func cleanUp(t *testing.T) {
+    // Cleanup table
+    _, err := db.Exec(`TRUNCATE stored_orders;`)
+    if err != nil {
+        t.Fatalf("cleanUp(): error truncating data in table %v", err)
+    }
+    _, err = db.Exec(`DELETE FROM stored_orders;`)
+    if err != nil {
+        t.Fatalf("cleanUp(): error deleting data in table %v", err)
+    }
 }
